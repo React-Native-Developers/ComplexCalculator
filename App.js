0d00ae15d1d6f65f29a16a471eae190f09c6ff95
Diego Alysson Braga Moreira <diegoalyssonbm@gmail.com>
2018-04-20 01:13:40 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import App from './src/App';
+
+export default class App7 extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
